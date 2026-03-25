3e31abbe49edbaef590e6af42649d44a948a08da
AgusRey04 <reymundez37@gmail.com>
2026-03-25 11:16:28 -0300

    agrega el ej2 resuelto

---
+++ reymundez/ej2/Program.cs
@@ -0,0 +1,9 @@
+﻿//2) Pedir por parámetro un nombre de persona y el nombre de una ciudad (no hace falta que 
+// sean reales o comprobarlos) y mostrar por consola el siguiente mensaje “Hola ” <nombre> 
+// ” bienvenido a ” <ciudad>”. Utilizar Interpolación. 
+
+Console.WriteLine("Ingresar su nombre:");
+string nombre = Console.ReadLine();
+Console.WriteLine("Ingresar el nombre de una ciudad:");
+string ciudad = Console.ReadLine();
+Console.WriteLine($"Hola {nombre}, bienvenido a {ciudad}.");
