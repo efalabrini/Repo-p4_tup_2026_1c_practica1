dfd2ab38a3676642004e7931498761eb073ab5f4
andy <andyvolpe21@hotmail.com>
2026-03-26 12:02:48 -0300

    realizacion de ej2

---
+++ avolpe/ej2/Program.cs
@@ -0,0 +1,9 @@
+﻿//ej2
+
+string nombre;
+string ciudad;
+Console.WriteLine("ingrese su nombre");
+nombre=Console.ReadLine();
+Console.WriteLine("ingrese ciudad");
+ciudad=Console.ReadLine();
+Console.WriteLine($"hola {nombre}, bienvenido a {ciudad}");
