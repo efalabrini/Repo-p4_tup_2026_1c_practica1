02327d10ac4cd1ef6211180290f2fa8129cdb73d
IvoBertoni89 <bertoniivox@gmail.com>
2026-03-30 10:51:43 -0300

    Ejs del 1 al 10
    todos los ejercicios resueltos del 1 al 10

---
+++ bertoni_ivo/ej1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Ejercicio 1
+
+int N1 = 67;
+int N2 = 14;
+int N3 = 12;
+
+int SUM;
+
+SUM = N1 + N2 + N3;
+
+Console.WriteLine($"La suma es: {SUM}");
---
+++ bertoni_ivo/ej2/Program.cs
@@ -0,0 +1,9 @@
+﻿//Ejercicio 2
+
+Console.WriteLine($"Ingrese su nombre: ");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Ingrese una ciudad: ");
+string city = Console.ReadLine();
+
+Console.WriteLine($"Hola {name}, bienvenido a {city}");
